c6adef0bb7cf2effe4a9510e92b9bacf4853c465
Leo Nieto <leonieto11@gmail.com>
2024-08-12 06:06:58 -0300

    js actualizado

--- app.js
+++ app.js
@@ -4,107 +4,114 @@ const Copiar = document.querySelector(".btn-copiar");
 const Muñeco = document.querySelector(".muñeco");
 const Alerta = document.querySelector(".alerta");
 const Instrucciones = document.querySelector(".instrucciones");
-const Restricciones = document.querySelector(".restricciones")
+const Restricciones = document.querySelector(".restricciones");
+const BtnLimpiar = document.querySelector(".btn-limpiar");
 
-// La letra "e" es convertida para "enter"
-// La letra "i" es convertida para "imes"
-// La letra "a" es convertida para "ai"
-// La letra "o" es convertida para "ober"
-// La letra "u" es convertida para "ufat"
-
-function aplicarAnimacion() {
-    Restricciones.classList.remove("animar"); // Elimina la clase para reiniciar la animación
-    void Restricciones.offsetWidth; // Forza la reflujo del DOM
-    Restricciones.classList.add("animar"); // Vuelve a agregar la clase para reiniciar la animación
+// Función para verificar y mostrar el botón Limpiar y ocultar otros elementos
+function actualizarBotonLimpiar() {
+    if (TextArea.value.trim() !== "") {
+        BtnLimpiar.style.display = "block";
+        Muñeco.style.display = "none";
+        Alerta.style.display = "none";
+        Instrucciones.style.display = "none";
+    } else {
+        BtnLimpiar.style.display = "none";
+        Muñeco.style.display = "block";
+        Alerta.style.display = "block";
+        Instrucciones.style.display = "block";
+    }
 }
 
 function btnEncriptar() {
-    const texto = TextArea.value.trim();
-    
-    if (texto === "") {
+    if (TextArea.value.trim() === "") {
         Restricciones.textContent = "El campo no debe estar vacío";
-        aplicarAnimacion(); // Aplica la animación
+        Restricciones.classList.add("restricciones--animacion");
         return;
     }
 
-    if (/[A-ZÁÉÍÓÚÑáéíóúñ]/.test(texto)) {
+    if (/[A-ZÁÉÍÓÚÜÑ]/.test(TextArea.value)) {
         Restricciones.textContent = "No se aceptan mayúsculas ni acentos, por favor inserte formato válido";
-        aplicarAnimacion(); // Aplica la animación
+        Restricciones.classList.add("restricciones--animacion");
+        TextArea.value = TextArea.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Elimina acentos
         return;
     }
 
-    const textoEncriptado = encriptar(texto);
+    const textoEncriptado = encriptar(TextArea.value);
     Mensaje.value = textoEncriptado;
-    TextArea.value = "";
     Copiar.style.display = "block";
     Mensaje.style.display = "block";
-    Muñeco.style.display = "none";
-    Alerta.style.display = "none";
-    Instrucciones.style.display = "none";
-    Restricciones.textContent = ""; // Limpia el mensaje de restricciones
-    Restricciones.classList.remove("animar"); // Remueve la clase de animación si se limpia el mensaje
-}
-
-function encriptar(stringEncriptada){
-    let matrizCodigo = [["e", "enter"], ["i","imes"], ["o","ober"], ["u", "ufat"], ["a", "ai"]];
-    stringEncriptada = stringEncriptada.toLowerCase();
-
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringEncriptada.includes(matrizCodigo[i][0])){
-            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
-        }
-    }
-    return stringEncriptada
+    Restricciones.textContent = "Solo letras minúsculas y sin acentos";
+    Restricciones.classList.remove("restricciones--animacion");
+    actualizarBotonLimpiar();
 }
 
 function btnDesencriptar() {
-    const texto = TextArea.value.trim();
-    
-    if (texto === "") {
+    if (TextArea.value.trim() === "") {
         Restricciones.textContent = "El campo no debe estar vacío";
-        aplicarAnimacion(); // Aplica la animación
+        Restricciones.classList.add("restricciones--animacion");
         return;
     }
 
-    if (/[A-ZÁÉÍÓÚÑáéíóúñ]/.test(texto)) {
+    if (/[A-ZÁÉÍÓÚÜÑ]/.test(TextArea.value)) {
         Restricciones.textContent = "No se aceptan mayúsculas ni acentos, por favor inserte formato válido";
-        aplicarAnimacion(); // Aplica la animación
+        Restricciones.classList.add("restricciones--animacion");
+        TextArea.value = TextArea.value.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, ""); // Elimina acentos
         return;
     }
 
-    const textoDesencriptado = desencriptar(texto);
+    const textoDesencriptado = desencriptar(TextArea.value);
     Mensaje.value = textoDesencriptado;
-    TextArea.value = "";
     Copiar.style.display = "block";
     Mensaje.style.display = "block";
-    Muñeco.style.display = "none";
-    Alerta.style.display = "none";
-    Instrucciones.style.display = "none";
-    Restricciones.textContent = ""; // Limpia el mensaje de restricciones
-    Restricciones.classList.remove("animar"); // Remueve la clase de animación si se limpia el mensaje
+    Restricciones.textContent = "Solo letras minúsculas y sin acentos";
+    Restricciones.classList.remove("restricciones--animacion");
+    actualizarBotonLimpiar();
 }
 
-function desencriptar(stringDesencriptada){
-    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i","imes"], ["o","ober"], ["u", "ufat"]];
+function btnLimpiar() {
+    TextArea.value = "";
+    Mensaje.value = "";
+    Copiar.style.display = "none";
+    Restricciones.textContent = "Solo letras minúsculas y sin acentos";
+    Restricciones.classList.remove("restricciones--animacion");
+    BtnLimpiar.style.display = "none";
+    actualizarBotonLimpiar();
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"], ["a", "ai"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["a", "ai"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]];
     stringDesencriptada = stringDesencriptada.toLowerCase();
 
-    for(let i = 0; i < matrizCodigo.length; i++){
-        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
             stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
         }
     }
-    return stringDesencriptada
+    return stringDesencriptada;
 }
 
-function btnCopiar(){
-    console.log(Mensaje.value);
-    navigator.clipboard.writeText(Mensaje.value).then(function(){
+function btnCopiar() {
+    navigator.clipboard.writeText(Mensaje.value).then(function() {
         console.log("Texto copiado");
-    }).catch(function(error){
+    }).catch(function(error) {
         console.error("Error al copiar el texto: ", error);
     });
-    } 
-    
+}
+
 Copiar.addEventListener("click", btnCopiar);
 
-
+// Verifica si el campo de texto tiene contenido al cargar la página
+document.addEventListener("DOMContentLoaded", actualizarBotonLimpiar);
+TextArea.addEventListener("input", actualizarBotonLimpiar);
